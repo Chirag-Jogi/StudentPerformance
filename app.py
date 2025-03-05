1761a0897d3a51f7103f0797b8c0b6bddcc0558f
Chirag Jogi <chiragjogi97@gmail.com>
2025-03-05 18:50:55 +0530

    app.py updated

--- app.py
+++ app.py
@@ -8,12 +8,7 @@ application=Flask(__name__)
 
 app=application
 ## Route for a home page
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def predict_datapoint():
     if request.method=='GET':
         return render_template('home.html')
@@ -24,15 +19,19 @@ def predict_datapoint():
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
         )
         pred_df=data.get_data_as_data_frame()
         print(pred_df)
 
         predict_pipeline=PredictPipeline()
         results=predict_pipeline.predict(pred_df)
+        print(results)
         return render_template('home.html',results=results[0])
+
+
+
     
-if __name__=="__main__":
-    app.run(host="0.0.0.0")    
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8000) 
